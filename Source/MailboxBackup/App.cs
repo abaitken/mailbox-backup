c6ea4eea63d9dd2751374c3f29bd167a8c769bb7
Alex Boyne-Aitken <abaitken@live.co.uk>
2021-02-04 10:54:46 +0000

    Initial commit. Scaffolded project

---
+++ Source/MailboxBackup/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MailboxBackup
+{
+    internal class App
+    {
+        public App()
+        {
+        }
+
+        internal int Run(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
